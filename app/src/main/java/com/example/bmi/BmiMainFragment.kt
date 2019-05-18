9e9e96fdcbd7210cf5cbc292c8e305ec716bae5a
Choi sungkyu <maroon92@naver.com>
2019-05-18 19:27:13 +0900

    bundle 대신에 bundleOf 사용

--- app/src/main/java/com/example/bmi/BmiMainFragment.kt
+++ app/src/main/java/com/example/bmi/BmiMainFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.fragment_bmi_main.*
@@ -26,9 +27,12 @@ class BmiMainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         button_bmi.setOnClickListener {
-            val bundle = Bundle()
-            bundle.putDouble("height", edit_text_height.text.toString().toDouble())
-            bundle.putDouble("height", edit_text_weight.text.toString().toDouble())
+            val bundle = bundleOf(
+                "height" to edit_text_height.text.toString().toDouble(),
+                "weight" to edit_text_weight.text.toString().toDouble()
+            )
+//            bundle.putDouble("height", edit_text_height.text.toString().toDouble())
+//            bundle.putDouble("height", edit_text_weight.text.toString().toDouble())
 
             it.findNavController().navigate(R.id.action_bmiMainFragment_to_bmiMain2Fragment, bundle)
         }
