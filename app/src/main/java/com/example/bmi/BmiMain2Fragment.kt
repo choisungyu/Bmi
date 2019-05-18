826597eeab38a6c55fe63a2ac79d67c073827a95
Choi sungkyu <maroon92@naver.com>
2019-05-18 21:10:34 +0900

    preference 를 자바 처럼 저장

--- app/src/main/java/com/example/bmi/BmiMain2Fragment.kt
+++ app/src/main/java/com/example/bmi/BmiMain2Fragment.kt
@@ -1,7 +1,9 @@
 package com.example.bmi
 
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -14,7 +16,7 @@ import kotlinx.android.synthetic.main.fragment_bmi_main2.*
 
 class BmiMain2Fragment : Fragment() {
 
-    val args: BmiMain2FragmentArgs by navArgs()
+    private val args: BmiMain2FragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,15 +37,25 @@ class BmiMain2Fragment : Fragment() {
         toast("$bmi")
 
         when {
-            bmi >= 35 -> text_view_result.text ="고도비만"
-            bmi >= 30 -> text_view_result.text ="2단계비만"
-            bmi >= 25 -> text_view_result.text ="1단계비만"
-            bmi >= 23 -> text_view_result.text ="과체중"
-            bmi >= 18.5 -> text_view_result.text ="정상"
-            else -> text_view_result.text ="저체중"
+            bmi >= 35 -> text_view_result.text = "고도비만"
+            bmi >= 30 -> text_view_result.text = "2단계비만"
+            bmi >= 25 -> text_view_result.text = "1단계비만"
+            bmi >= 23 -> text_view_result.text = "과체중"
+            bmi >= 18.5 -> text_view_result.text = "정상"
+            else -> text_view_result.text = "저체중"
         }
 
-        Log.d("BmiMain2Fragment","$args.height, $args.weight")
+
+        val preference: SharedPreferences =
+            PreferenceManager.getDefaultSharedPreferences(requireContext())
+
+        val editor = preference.edit()
+        editor.putFloat("height", args.height)
+            .putFloat("weight", args.weight)
+            .apply()
+
+
+        Log.d("BmiMain2Fragment", "$args.height, $args.weight")
 
     }
 
