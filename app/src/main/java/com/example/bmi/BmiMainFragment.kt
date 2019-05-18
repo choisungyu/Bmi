36368700793227e92a21ebbc34534845e5aa0531
Choi sungkyu <maroon92@naver.com>
2019-05-18 18:40:45 +0900

    navigate 하는거

--- app/src/main/java/com/example/bmi/BmiMainFragment.kt
+++ app/src/main/java/com/example/bmi/BmiMainFragment.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 
 class BmiMainFragment : Fragment() {
 
@@ -15,17 +17,13 @@ class BmiMainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
+        val view = inflater.inflate(R.layout.fragment_bmi_main, container, false)
+        val button = view.findViewById<Button>(R.id.button_bmi)
 
-        return inflater.inflate(R.layout.fragment_bmi_main, container, false)
-
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-
+        button.setOnClickListener {
+            it.findNavController().navigate(R.id.action_bmiMainFragment_to_bmiMain2Fragment)
+        }
+        return view
 
     }
-
-
 }
--- app/src/main/java/com/example/bmi/MainActivity.kt
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
