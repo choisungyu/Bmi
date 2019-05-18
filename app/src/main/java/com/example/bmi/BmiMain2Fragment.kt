3865d350d882ac149ab73c3d5a258224a9ee1fc3
Choi sungkyu <maroon92@naver.com>
2019-05-18 20:34:18 +0900

    확장 함수 toast 를 만들어서 MyExtension File 에 넣어서 static 으로 어디서든 쓸 수 있게 만들기

--- app/src/main/java/com/example/bmi/BmiMain2Fragment.kt
+++ app/src/main/java/com/example/bmi/BmiMain2Fragment.kt
@@ -7,7 +7,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_bmi_main2.*
 
 
 class BmiMain2Fragment : Fragment() {
@@ -27,6 +29,19 @@ class BmiMain2Fragment : Fragment() {
 
 //        val height = arguments?.getDouble("height")
 //        val weight = arguments?.getDouble("weight")
+
+        val bmi = args.weight / Math.pow(args.height / 100.0, 2.0)
+
+        toast("$bmi")
+
+        when {
+            bmi >= 35 -> text_view_result.text ="고도비만"
+            bmi >= 30 -> text_view_result.text ="2단계비만"
+            bmi >= 25 -> text_view_result.text ="1단계비만"
+            bmi >= 23 -> text_view_result.text ="과체중"
+            bmi >= 18.5 -> text_view_result.text ="정상"
+            else -> text_view_result.text ="저체중"
+        }
 
         Log.d("BmiMain2Fragment","$args.height, $args.weight")
 
--- app/src/main/java/com/example/bmi/BmiMainFragment.kt
+++ app/src/main/java/com/example/bmi/BmiMainFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import kotlinx.android.synthetic.main.fragment_bmi_main.*
@@ -34,10 +33,14 @@ class BmiMainFragment : Fragment() {
 //            bundle.putDouble("height", edit_text_height.text.toString().toDouble())
 //            bundle.putDouble("height", edit_text_weight.text.toString().toDouble())
 
+
+
             val action = BmiMainFragmentDirections.actionBmiMainFragmentToBmiMain2Fragment(
                 edit_text_height.text.toString().toFloat(),
                 edit_text_weight.text.toString().toFloat()
             )
+            // 확장함수
+            toast("hello")
 
 //            it.findNavController().navigate(R.id.action_bmiMainFragment_to_bmiMain2Fragment, bundle)
             it.findNavController().navigate(action)
